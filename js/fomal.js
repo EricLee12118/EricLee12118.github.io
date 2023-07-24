cee9162adf57c8d70f9195db47807ec6f9903af8
EricLee12118 <yongjie12118@gmail.com>
2023-07-24 09:30:22 +0800

    Site updated: 2023-07-24 09:30:22

--- js/fomal.js
+++ js/fomal.js
@@ -1321,7 +1321,7 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
   if (document.hidden) {
     //离开当前页面时标签显示内容
-    document.title = '👀跑哪里去了~';
+    document.title = '正在前往其他页面';
     clearTimeout(titleTime);
   } else {
     //返回当前页面时标签显示内容
